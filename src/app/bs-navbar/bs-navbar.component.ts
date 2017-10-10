78abbe489ec4abe044ea8ce02157ef772492b840
Fred Wong <fredwong.it@gmail.com>
2017-10-10 12:07:46 -0400

    Implementing the Logout
    add the logout function to the Log Out button
    call the AngularFireAuth signOut function to logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(x => console.log(x));
   }
 
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
