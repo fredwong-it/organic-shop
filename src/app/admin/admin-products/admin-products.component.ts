d385f1817bf10d1c2ff8fdf631457979f01aa2df
Fred Wong <fredwong.it@gmail.com>
2017-10-16 16:49:27 -0400

    Searching for Products
    create the filter method to filter the products
    use the filteredProducts instead of products$
    subscribe and unsubscribe the observable to avoid the memory leak

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../../product.service';
 
 @Component({
@@ -6,14 +7,32 @@ import { ProductService } from '../../product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: { title: string }[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    // don't apply the take one here because user may open 2 screen with products list and edit product screen
+    // we want to keep subscribe the changes of the getAll() observable
+    // so we need to unsubscribe the observable on the ngOnDestroy
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);     // set the filteredProducts at the first time
   }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    console.log(query);
+
+    // either filter the products or display all products
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
 }
