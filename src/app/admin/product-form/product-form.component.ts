bd7e2e81fa7c45845684a3989ebca34014e68c7c
Fred Wong <fredwong.it@gmail.com>
2017-10-16 13:33:08 -0400

    Updating a Product
    create update method in ProductService
    add condition for create and update method when use click save button

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -10,9 +10,9 @@ import 'rxjs/add/operator/take';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-
   categories$;
   product = {};
+  id;
 
   constructor(
     categoryService: CategoryService,
@@ -23,20 +23,26 @@ export class ProductFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get('id');
 
-    if (id) {
+    if (this.id) {
       // take operator will only take 1 value from the observable and it will automatically be completed
       // we don't need to explicity unsubscribe because the observable won't emit any new value
-      this.productService.get(id).take(1).subscribe(p => this.product = p);
+      this.productService.get(this.id).take(1).subscribe(p => this.product = p);
     }
 
-    console.log(id);
+    console.log(this.id);
   }
 
   save(product) {
     console.log(product);
-    this.productService.create(product);
+
+    if (this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
+
     this.router.navigate(['/admin/products']);
   }
 }
