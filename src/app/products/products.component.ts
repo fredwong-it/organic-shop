e0a4be14ebfaff7373e9d27b7ff3989626ca4d15
Fred Wong <fredwong.it@gmail.com>
2017-10-17 15:47:56 -0400

    Displaying All the Products
    use the bootstrap card to display the product
    use the bootstrap col and w-100 class for the 2 column display

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
+  }
 
   ngOnInit() {
   }
