c85d8fe530d429d5b3f1f3b6bcc800fe322f9d0b
Fred Wong <fredwong.it@gmail.com>
2017-10-17 16:49:22 -0400

    Filtering Products by Category
    add routerLink to current page with the query parameter
    subscribe queryParamMap observable for the changing of the route
    filter the product by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,45 @@
+import { ActivatedRoute } from '@angular/router';
+import { Product } from './../models/product';
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
-  products$;
+export class ProductsComponent implements OnInit, OnDestroy {
   categories$;
+  products: Product[] = [];
+  filteredProducts: Product[];
+  subscription: Subscription;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
     this.categories$ = categoryService.getAll();
+    this.subscription = productService.getAll()
+      .subscribe(products => this.products = products);
+
+    // can't use snapshot because the component won't be destroy when user choose different category
+    // it needs to subscribe the category observable to display different category route
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+    });
   }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
