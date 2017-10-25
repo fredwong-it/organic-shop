206b9e1444af718711366912e1a688ee26710ef6
Fred Wong <fredwong.it@gmail.com>
2017-10-25 14:57:27 -0400

    Displaying the Number of Shopping Cart Items in NavBar
    create shopping-cart and shopping-cart-item model
    subscribe the cart$ FirebaseObservable

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from '../shopping-cart.service';
 import { AuthService } from './../auth.service';
 import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
@@ -8,13 +9,27 @@ import { AppUser } from '../models/app-user';
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  shoppingCartItemCount: number;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private cartService: ShoppingCartService) {
+  }
+
+  async ngOnInit() {
     // only 1 instance for this component in the application so there won't be a memory leak
     // no need to unsubscribe this observable
     this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+
+    const cart$ = await this.cartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+
+      // calculate the shoppingCartItemCount
+      for (const productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    });
   }
 
   logout() {
