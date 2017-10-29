6ff1b70e19111a785222c2a2a2060e2323111ee7
Fred Wong <fredwong.it@gmail.com>
2017-10-29 19:16:01 -0400

    Associateing the Order with the Current User
    get the userId from the AuthService
    unsubscribe both cart and user subscription

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -4,6 +4,7 @@ import { Subscribable } from 'rxjs/Observable';
 import { ShoppingCart } from '../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-check-out',
@@ -12,27 +13,33 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit, OnDestroy {
   shipping = {};
+  userId: string;
   cart: ShoppingCart;
-  subscription: Subscription;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
     private cartService: ShoppingCartService,
-    private orderService: OrderService) {
+    private orderService: OrderService,
+    private authService: AuthService) {
   }
 
   async ngOnInit() {
     const cart$ = await this.cartService.getCart();
-    this.subscription = cart$.subscribe(cart => this.cart = cart);
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder() {
     console.log(this.shipping);
 
     const order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       // map to the order items from shopping-cart item
