5411deb0d7197e426fd1b2d7162041e455ed0833
Fred Wong <fredwong.it@gmail.com>
2017-11-05 22:26:21 -0500

    Refactoring: Extract ShippingFormComponent

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,52 +1,20 @@
-import { OrderService } from '../order.service';
-import { Subscription } from 'rxjs/Rx';
-import { Subscribable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { ShoppingCart } from '../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { Order } from '../models/order';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
-  shipping = {};
-  userId: string;
-  cart: ShoppingCart;
-  cartSubscription: Subscription;
-  userSubscription: Subscription;
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor(
-    private router: Router,
-    private cartService: ShoppingCartService,
-    private orderService: OrderService,
-    private authService: AuthService) {
+  constructor(private cartService: ShoppingCartService) {
   }
 
   async ngOnInit() {
-    const cart$ = await this.cartService.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
-  }
-
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
-  }
-
-  async placeOrder() {
-    //console.log(this.shipping);
-    const order = new Order(this.userId, this.shipping, this.cart);
-
-    // storeOrder return a promise, either use then or await to get the result
-    const result = await this.orderService.placeOrder(order);
-
-    // $key - read a node from firebase
-    // key - when you store something in firebase, firebase returns these newly generated ID in this key property
-    this.router.navigate(['/order-success', result.key]);
+    this.cart$ = await this.cartService.getCart();
   }
 }
---
+++ src/app/shipping-form/shipping-form.component.ts
@@ -0,0 +1,45 @@
+import { Subscription } from 'rxjs/Rx';
+import { AuthService } from './../auth.service';
+import { OrderService } from './../order.service';
+import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Order } from '../models/order';
+import { ShoppingCart } from '../models/shopping-cart';
+
+@Component({
+  selector: 'shipping-form',
+  templateUrl: './shipping-form.component.html',
+  styleUrls: ['./shipping-form.component.css']
+})
+export class ShippingFormComponent implements OnInit, OnDestroy {
+  @Input('cart') cart: ShoppingCart;
+  shipping = {};
+  userId: string;
+  subscription: Subscription;
+
+  constructor(
+    private router: Router,
+    private orderService: OrderService,
+    private authService: AuthService) {
+  }
+
+  async ngOnInit() {
+    this.subscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+  }
+
+  async placeOrder() {
+    //console.log(this.shipping);
+    const order = new Order(this.userId, this.shipping, this.cart);
+
+    // storeOrder return a promise, either use then or await to get the result
+    const result = await this.orderService.placeOrder(order);
+
+    // $key - read a node from firebase
+    // key - when you store something in firebase, firebase returns these newly generated ID in this key property
+    this.router.navigate(['/order-success', result.key]);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
